366f16d551c72c77d0f382bacd54873557fa2022
ghiyasFazle <ghiyasfazle30@gmail.com>
2025-10-11 19:57:06 +0700

    dev state after tugas 6

--- main/models.py
+++ main/models.py
@@ -20,7 +20,6 @@ class product(models.Model):
     price = models.IntegerField()
     stock = models.IntegerField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
-    # Allow longer thumbnails (some CDNs or signed URLs can exceed 200 chars)
     thumbnail = models.URLField(max_length=2000, blank=True, null=True)
     is_sale = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
